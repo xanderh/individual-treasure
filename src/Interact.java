4542efe95c76e473b826ebc5bc858b39f3a995e6
Alexander Hvilshøj <alexander@hvilshoej.dk>
2016-12-31 03:30:35 +0100

    Added basic generation

--- src/Interact.java
+++ src/Interact.java
@@ -1,4 +1,22 @@
 
 public class Interact {
-	
+
+	public static void initialize() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void generate(String[] args) {
+		int generated = 0;
+		int results[] = new int[5];
+
+		while (generated < args.length) {
+			int result[] = Roller.Roll(Integer.parseInt(args[generated++]), Integer.parseInt(args[generated++]));
+			for (int i = 0; i < results.length; i++) {
+				results[i] = results[i] + result[i];
+			}
+		}
+		System.out.println(results[0] + " copper, " + results[1] + " silver, " + results[2] + " electrum, " + results[3] + " gold, " + results[4] + " platinum.");
+	}
+
 }
