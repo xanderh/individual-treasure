148407b0e5c71f3cafe4b8871b8376976bdfba26
Alexander Hvilshøj <alexander@hvilshoej.dk>
2016-12-31 03:29:32 +0100

    Added main and help functions

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,26 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		if(args.length == 0) {
+			InteractGUI.initialize();
+		} else if (args[0] == "-nogui") {
+			Interact.initialize();
+		} else if (args.length % 2 == 0) {
+			Interact.generate(args);
+		} else if (args[0] == "help") {
+			help();
+		} else {
+			System.out.println("badly formed argument, program closing");
+		}
 
+	}
+	
+	public static void help() {
+		System.out.println("This program is designed for easily generating treasure found on random enemies in D&D 5e.");
+		System.out.println("Run the program without arguments for a GUI");
+		System.out.println("Run the program with the -nogui argument to get a command-line prompt");
+		System.out.println("Run the program with an arbitrary amount of pairs of arguments to receive a single "
+				+ "generated total amount. These pairs must be the level of the enemy, followed by the amount");
 	}
 
 }
